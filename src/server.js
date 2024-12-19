fb312789ecef46ad5bace7eae1d856c41489b6ea
Kyungseok Oh <bestspx@gmail.com>
2024-12-19 10:48:40 +0900

    Chat Completed

--- src/server.js
+++ src/server.js
@@ -12,13 +12,27 @@ app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:${PORT}`);
-function handleConnection (socket) {
-  console.log(socket);
-}
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-wss.on('connection', handleConnection)
+const sockets = [];
+
+function onSocketClose() {
+  console.log("Disconnected from the Browser ❌");
+}
+
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  console.log("Connected to Browser ✅");
+  socket.on("close", onSocketClose);
+  socket.on("message", (event) => {
+    const message = event.toString();
+    sockets.forEach((eachSocket) => {
+      eachSocket.send(message);
+    });
+  });
+  socket.send("hello!!!");
+});
 
 server.listen(PORT, handleListen);
